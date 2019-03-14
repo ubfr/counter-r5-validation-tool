6c29bdc5ce79547e7bbd268977b37591c4104976
Christian Jägle <christian.jaegle@ub.uni-freiburg.de>
2019-03-14 14:28:34 +0100

    Adapt changes made in duplicated provider users model

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'first_name','last_name','display_name', 'email', 'password','utype',
+        'first_name','last_name','display_name', 'email', 'password','newsletter','commercial','utype','gender'
     ];
 
     /**
